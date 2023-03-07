6d9081b9bf4bfe30fae08496dca98639efb6a1e4
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-07 19:52:20 +0530

    upates

---
+++ advancejs.js
@@ -0,0 +1,22 @@
+//var - redeclarstion and reassign is possible
+// var myName = "awdiz";
+console.log( myName);
+myName = "abc";
+console.log( myName);
+var myName = "xyz";
+console.log( myName);
+
+// let - reassign is possible but redeclaretion is not possiable
+let namee = "awdiz";
+console.log( namee);
+namee = "abc";
+console.log( namee);
+// let namee = "xyz";
+// console.log( namee);
+
+// const-redeclarstion and reassign not possible
+// const age = 20;
+// console.log (age)
+
+// const age = 40;
+// console.log (age)
--- javascriptQ.js
+++ javascriptQ.js
@@ -2,8 +2,8 @@
 
 // Array.includes(23);
 // console.log(Array.includes);
-var  array = [1,2,443,454,232,546,23,567,676,2342,565,4,2,6]
-var number = 6;
+// var  array = [1,2,443,454,232,546,23,567,676,2342,565,4,2,6]
+// var number = 6;
 // console.log(array.includes(23))
 // if (array.includes(number)){
 //     console.log("true")
@@ -29,22 +29,33 @@ var number = 6;
 // Q find out number 689  from given array
  // then print only " got it "  and if not found then print " not found " 
 
-var array = [2,4,6,23,56,3224,454,23,54,56,232,689,233];
-var number = 689;
+// var array = [2,4,6,23,56,3224,454,23,54,56,232,689,233];
+// var number = 689;
 
-var flag = flag;
-for (var i= 0; i < array.length; i++){
-   if (array[i] === number){
-    flag = true;
-   }
+// var flag = flag;
+// for (var i= 0; i < array.length; i++){
+//    if (array[i] === number){
+//     flag = true;
+//    }
    
      
-}
-if (flag === true){
-    console.log(" got it")
-}
-else{
-    console.log("not found")
-}
+// }
+// if (flag === true){
+//     console.log(" got it")
+// }
+// else{
+//     console.log("not found")
+// }
 
+// given an array make an  object with key of that specific index with value;
+var array = [9,8,7,6,5,4,3,2,1,0]
 
+// var result = { 0: array[0],  1: array[9], 2: array[8], 3: array[7], 4: array[6], 5: array[5], 6: array[4], 7: array[3], 8: array[2], 9: array[1],  }
+// console.log(result)
+
+var result = {};
+
+for(var i=0; i<array.length; i++){
+    result[i] = array[i].toString();
+}
+console.log(result);
